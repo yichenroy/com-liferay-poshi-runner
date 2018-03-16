ec4e8aca0a206aaeaf3966e811b09fe0e1dd91f7
yichenroy <yi-chen.tsai@liferay.com>
2018-03-16 15:00:32 -0700

    LRQA-35911 Add TransposeElement

---
+++ poshi-runner/src/main/java/com/liferay/poshi/runner/elements/TransposeElement.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.elements;
+
+import org.dom4j.Element;
+import org.dom4j.tree.DefaultElement;
+
+/**
+ * @author Yi-Chen Tsai
+ */
+public class TransposeElement extends DefaultElement {
+
+	public TransposeElement(
+		Element baseElement, Element overrideElement,
+		String overrideNamespacedClassName) {
+
+		super("");
+
+		_baseElement = baseElement;
+		_baseNamespacedClassName = overrideElement.attributeValue("override");
+		_overrideElement = overrideElement;
+		_overrideNamespacedClassName = overrideNamespacedClassName;
+
+		_transpose();
+	}
+
+	public Element getBaseElementCopy() {
+		return _baseElement.createCopy();
+	}
+
+	public String getBaseNamespacedClassName() {
+		return _baseNamespacedClassName;
+	}
+
+	public Element getOverrideElementCopy() {
+		return _overrideElement.createCopy();
+	}
+
+	public String getOverrideNamespacedClassName() {
+		return _overrideNamespacedClassName;
+	}
+
+	private void _transpose() {
+		Element baseElement = getBaseElementCopy();
+
+		setQName(baseElement.getQName());
+
+		setAttributes(baseElement.attributes());
+
+		setContent(baseElement.content());
+	}
+
+	private final Element _baseElement;
+	private final String _baseNamespacedClassName;
+	private final Element _overrideElement;
+	private final String _overrideNamespacedClassName;
+
+}
