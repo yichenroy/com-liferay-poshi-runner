daf788c428fe203ab279fa6176eb24515547ad22
Leslie Wong <leslie.wong@liferay.com>
2018-07-12 10:43:52 -0700

    LRQA-42319 Log nested return elements

--- poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerExecutor.java
+++ poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerExecutor.java
@@ -824,6 +824,8 @@ public class PoshiRunnerExecutor {
 					returnName, _macroReturnValue);
 
 				_macroReturnValue = null;
+
+				PoshiElementLogger.pass(returnElement);
 			}
 		}
 		catch (Exception e) {
@@ -864,6 +866,8 @@ public class PoshiRunnerExecutor {
 			if (returnElement != null) {
 				PoshiRunnerVariablesUtil.putIntoCommandMap(
 					returnElement.attributeValue("name"), returnValue);
+
+				PoshiElementLogger.pass(returnElement);
 			}
 
 			CommandLoggerHandler.logExternalMethodCommand(
